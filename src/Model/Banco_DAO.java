eeb9b123bf646e85d6599dbc488fa08f4e4991ad
GustavoVFA <gustavovfamorim@gmail.com>
2023-04-11 22:06:58 -0300

    Banco

---
+++ src/Model/Banco_DAO.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Banco_DAO {
+    static String nome;
+    static String end;
+    static String email;
+    public static void juntaInfo(){
+        System.out.println("Nome: "+nome+"\nEnd: "+end+"\nEmail: "+email);
+    }
+}

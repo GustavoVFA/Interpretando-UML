9e7a1520aa143f6fd0dba40ec4f3f091b12ce989
GustavoVFA <gustavovfamorim@gmail.com>
2023-04-11 22:07:13 -0300

    Menu

---
+++ src/Model/Menu_DAO.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class Menu_DAO {
+    public static void dados(){
+        Model.Banco_DAO.juntaInfo();
+    }
+    public static void info(){
+        Model.Banco_DAO.juntaInfo();
+    }
+    public static void resp(){
+        Model.Banco_DAO.juntaInfo();
+    }
+}
